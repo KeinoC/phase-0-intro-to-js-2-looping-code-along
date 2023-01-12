4544f8ada4231f1df7ad45c6b42a3aa2113f846d
Keino Chichester <keino.chichester@gmail.com>
2023-01-12 12:56:43 -0500

    looping lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards (list,eventType) {
+    let thankYous = []
+    for (let i = 0; i < list.length; i++) {
+    let thanks = `Thank you, ${list[i]}, for the wonderful ${eventType} gift!`
+    thankYous.push(thanks)
+    }
+return thankYous
+}
+
+function countDown(number) {
+    for (let i = number; i >=0; i--) {
+        console.log(i)
+}
+}
